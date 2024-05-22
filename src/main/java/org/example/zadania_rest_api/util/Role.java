d17f20b30396f931ee5263b581951a600a734991
gszp1 <grzybekmateusz@poczta.fm>
2024-05-23 01:18:16 +0200

    Permissions fixes.

--- src/main/java/org/example/zadania_rest_api/util/Role.java
+++ src/main/java/org/example/zadania_rest_api/util/Role.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Getter
@@ -44,9 +45,10 @@ public enum Role {
     public List<SimpleGrantedAuthority> getAuthorities() {
         var authorities = getPermissions()
                 .stream()
-                .map(permission -> new SimpleGrantedAuthority(permission.name()))
-                .toList();
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toList());
         authorities.add(new SimpleGrantedAuthority("ROLE_" + name()));
+        authorities.forEach(System.out::println);
         return authorities;
     }
 }
