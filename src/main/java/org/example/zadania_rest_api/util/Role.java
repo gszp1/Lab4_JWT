9d1f87968f44d03e648e17e442a2282cee66b529
gszp1 <grzybekmateusz@poczta.fm>
2024-05-22 22:41:16 +0200

    Adds permisions and roles.

--- src/main/java/org/example/zadania_rest_api/util/Role.java
+++ src/main/java/org/example/zadania_rest_api/util/Role.java
@@ -1,6 +1,42 @@
 package org.example.zadania_rest_api.util;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Getter
 public enum Role {
-    USER,
-    ADMIN
+    USER(
+            Set.of(
+                    Permission.USER_READ
+            )
+    ),
+    ADMIN(
+            Set.of(
+                    Permission.ADMIN_READ,
+                    Permission.ADMIN_UPDATE,
+                    Permission.ADMIN_DELETE,
+                    Permission.ADMIN_CREATE,
+                    Permission.USER_READ,
+                    Permission.USER_UPDATE,
+                    Permission.USER_DELETE,
+                    Permission.USER_CREATE
+            )
+    );
+
+    private final Set<Permission> permissions;
+
+    public List<SimpleGrantedAuthority> getAuthorities() {
+        var authorities = getPermissions()
+                .stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.name()))
+                .toList();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + name()));
+        return authorities;
+    }
 }
